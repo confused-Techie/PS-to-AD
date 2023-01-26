1e53e4c3018447dfabfbe109bc73372b138183d1
confused_techie <dev@lhbasics.com>
2023-01-26 07:47:41 -0800

    Removed `node-windows`

--- src/integrations/activedirectory.js
+++ src/integrations/activedirectory.js
@@ -2,39 +2,7 @@
  * @file Exposes the utilities to integrate with Active Directory
  */
 
-const win = require("node-windows");
 const child_process = require("node:child_process");
-
-/**
- * @function checkProcessStatus
- * @async
- * @desc A currently unused function used to check if the current process running NodeJS is an admin.
- * @returns {object} A server status object where `ok` is true or false based on success.
- */
-async function checkProcessStatus() {
-  // This should be used to ensure we have any permissions needed to run the process.
-  // Essentially checkin if we are in an admin process, or if we need to get ourselves
-  // into an admin process.
-
-  try {
-    win.isAdminUser((isAdmin) => {
-      if (isAdmin) {
-        return {
-          ok: true,
-          msg: "This Process is an administrative process.",
-        };
-      }
-      // We are not in an admin process, and should try and elvate it.
-      return {
-        ok: false,
-        msg: "This Process is NOT an Admin process.",
-      };
-    });
-  } catch (err) {
-    throw err;
-    process.exit(1);
-  }
-}
 
 /**
  * @function getStaffList
