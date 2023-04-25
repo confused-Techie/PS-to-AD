926b01543b43b543327f1d3624dd152d7d996c1f
confused-Techie <confused-Techie@users.noreply.github.com>
2023-04-25 15:17:38 +0000

    GH Action Standards

--- src/validate.js
+++ src/validate.js
@@ -1,39 +1,49 @@
 /**
-  * @file This module provides quick validation of an objects structure.
-  * ALlowing the rest of the program to quickly ensure it's able to work with the data
-  * provided by remote servers.
-  */
+ * @file This module provides quick validation of an objects structure.
+ * ALlowing the rest of the program to quickly ensure it's able to work with the data
+ * provided by remote servers.
+ */
 
 const Joi = require("joi");
 
 const active_directory = Joi.array().items(
-  Joi.object().keys({
-    "GivenName": Joi.string().required(),
-    "Surname": Joi.string().required(),
-    "Enabled": Joi.boolean().required(),
-    "SamAccountName": Joi.string().required()
-  }).unknown(true)
+  Joi.object()
+    .keys({
+      GivenName: Joi.string().required(),
+      Surname: Joi.string().required(),
+      Enabled: Joi.boolean().required(),
+      SamAccountName: Joi.string().required(),
+    })
+    .unknown(true)
 );
 
 const powerschool = Joi.array().items(
   Joi.object().keys({
-    details: Joi.object().keys({
-      staffs: Joi.object().keys({
-        staff: Joi.array().items(
-          Joi.object().keys({
-            users_dcid: Joi.number().integer().required(),
-            local_id: Joi.any().required(),
-            name: Joi.object().keys({
-              first_name: Joi.string().required(),
-              last_name: Joi.string().required()
-            }).unknown(true)
-          }).unknown(true)
-        )
-      }).unknown(true)
-    }).unknown(true),
+    details: Joi.object()
+      .keys({
+        staffs: Joi.object()
+          .keys({
+            staff: Joi.array().items(
+              Joi.object()
+                .keys({
+                  users_dcid: Joi.number().integer().required(),
+                  local_id: Joi.any().required(),
+                  name: Joi.object()
+                    .keys({
+                      first_name: Joi.string().required(),
+                      last_name: Joi.string().required(),
+                    })
+                    .unknown(true),
+                })
+                .unknown(true)
+            ),
+          })
+          .unknown(true),
+      })
+      .unknown(true),
     count: Joi.number().required(),
     schoolName: Joi.any().required(),
-    schoolID: Joi.any().optional()
+    schoolID: Joi.any().optional(),
   })
 );
 
