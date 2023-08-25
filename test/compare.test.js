ba7f05eef2236223147e977ab548863ef63c88b3
confused-Techie <confused-Techie@users.noreply.github.com>
2023-08-25 21:12:14 +0000

    GH Action Standards

--- test/compare.test.js
+++ test/compare.test.js
@@ -4,37 +4,35 @@ const assert = require("node:assert");
 const compare = require("../src/integrations/compare.js");
 
 describe("compare", () => {
-
   it("reports users who only exist in PowerSchool", async () => {
-
     const psData = [
       {
-        "details": {
-          "staffs": {
-            "staff": [
+        details: {
+          staffs: {
+            staff: [
               {
-                "id": 2,
-                "local_id": 3,
-                "admin_username": "thedev",
-                "name": {
-                  "first_name": "The",
-                  "last_name": "Developer"
+                id: 2,
+                local_id: 3,
+                admin_username: "thedev",
+                name: {
+                  first_name: "The",
+                  last_name: "Developer",
                 },
-                "users_dcid": 4
-              }
-            ]
-          }
-        }
-      }
+                users_dcid: 4,
+              },
+            ],
+          },
+        },
+      },
     ];
 
     const adData = [
       {
-        "GivenName": "t",
-        "Surname": "dev",
-        "SamAccountName": "testdev",
-        "Enabled": true
-      }
+        GivenName: "t",
+        Surname: "dev",
+        SamAccountName: "testdev",
+        Enabled: true,
+      },
     ];
 
     const config = {
@@ -43,35 +41,37 @@ describe("compare", () => {
         skipPS: false,
         skipAD: false,
         noWrite: true,
-        attribute: "extensionAttribute1"
-      }
+        attribute: "extensionAttribute1",
+      },
     };
 
     const res = await compare.compare(psData, adData, config);
 
-    assert.equal(res[0], "Not Found: (PowerSchool -> Active Directory) The, Developer; DCID: 4; Teacher Number: 3");
+    assert.equal(
+      res[0],
+      "Not Found: (PowerSchool -> Active Directory) The, Developer; DCID: 4; Teacher Number: 3"
+    );
   });
 
   it("reports users who only exist in Active Directory", async () => {
-
     const psData = [
       {
-        "details": {
-          "staffs": {
-            "staff": [
+        details: {
+          staffs: {
+            staff: [
               {
-                "local_id": 3,
-                "admin_username": "thedev",
-                "name": {
-                  "first_name": "The",
-                  "last_name": "Developer"
+                local_id: 3,
+                admin_username: "thedev",
+                name: {
+                  first_name: "The",
+                  last_name: "Developer",
                 },
-                "users_dcid": 4
-              }
-            ]
-          }
-        }
-      }
+                users_dcid: 4,
+              },
+            ],
+          },
+        },
+      },
     ];
   });
 });
